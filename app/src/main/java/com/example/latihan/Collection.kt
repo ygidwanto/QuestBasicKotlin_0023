0455439db61289deae90387448037c718d11ea76
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-10 20:49:00 +0700

    Commit 8: menambahkan fungsi dari List Ordered,mutable dan read only menggunakan listof, list mutbale menggunakan mutableListof

---
+++ app/src/main/java/com/example/latihan/Collection.kt
@@ -0,0 +1,40 @@
+package com.example.latihan
+
+//List
+// List  ordered
+//List  mutable dan read only
+//List Read-Only
+// List Mutable
+
+fun ContohList() {
+    println("=== List ===")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Muutable
+    shape.remove("Triangle")
+    println(shape)
+
+    //Mengubah data
+    shape[0] = "Oval"
+    println(shape)
+
+    //List Read Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
+
+//
+//
+//
+//
+//
