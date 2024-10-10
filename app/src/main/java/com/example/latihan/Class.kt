5f4442175a6b8a94fa63efe756a77ae4ef948837
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-10 20:09:02 +0700

    Commit 4: Menambahkan perintah print Contact Email

--- app/src/main/java/com/example/latihan/Class.kt
+++ app/src/main/java/com/example/latihan/Class.kt
@@ -5,5 +5,11 @@ class Motor()
 class Contact(val id: Int, var email: String)
 
 fun main() {
+val contact = Contact(1,"mary@gmail.com")
+
+    // Print the value of the property: Email
+    println(contact.email)
+
+
 
 }
