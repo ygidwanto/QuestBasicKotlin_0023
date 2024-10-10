04e75949e763b1c201a4ad939d6843e6fb5d8138
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-10 19:43:19 +0700

    Commit 1 (Publish Github)

---
+++ app/src/main/java/com/example/latihan/TipeData.kt
@@ -0,0 +1,43 @@
+package com.example.latihan
+
+fun main() {
+
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // long
+    val longValue: Long = 40
+    println("Long: $longValue")
+
+    //float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    // boolean
+    val booleanValue: Boolean = true
+    print("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello Word!"
+    println("String: $stringValue")
+
+    stringValue = "Hello Word!"
+    println("string : $stringValue")
+}
