e72213312064a660b4d396b242f6ed2438aa0c2f
ygidwanto <yogi.dewanto.ft22@mail.umy.ac.id>
2024-10-10 21:50:42 +0700

    Commit13 mengembalikan nilai berdasarkan kondisi tertentu

--- app/src/main/java/com/example/latihan/ConditionalExpression.kt
+++ app/src/main/java/com/example/latihan/ConditionalExpression.kt
@@ -1,6 +1,32 @@
 package com.example.latihan
 
-fun ConditionalStatement(){
-    println("=== Conditional Expression===")
-
+fun ConditionalStatement() {
+    println("==Conditional Expression ==")
+    //
+    val number = 10
+    if(number > 0) {
+        println("Positive Number")
+    }else {
+        println("Negative Number")
+    }
+    //
+    val day = 1
+    when (day){
+            1 -> println("Sunday")
+            2 -> println("Monday")
+            3 -> println("Tuesday")
+            4 -> println("Wednesday")
+            5 -> println("Thursday")
+            6 -> println("Friday")
+            7 -> println("Saturday")
+            else -> println("Invalid Day")
+    }
+    //
+    val value = "10"
+    try{
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+       println("Invalid Number")
+    }
 }
